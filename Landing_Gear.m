0274b286d7b26bdb728ed75e8fcf18482828ccc2
m95dragon <m95dragon@yahoo.com>
2017-03-05 19:39:05 -0800

    Added Landing Gear Script

---
+++ Landing_Gear.m
@@ -0,0 +1,9 @@
+% Landing Gear Preliminary Design 
+% Parameters to Determine:
+% Wheel Track, Wheel Base, Height, Sizing, Location, Load Distribution,
+% Tires
+%% META
+% 3/5/17
+
+%% Intial Conditions
+
