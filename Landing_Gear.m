887fb258c6fdfdbe8be860989b5bdf842a706cb1
mjbauer03 <mjbauer@ucdavis.edu>
2017-03-10 12:45:56 -0800

    Began work on ground clearance and tip-over requirements

--- Landing_Gear.m
+++ Landing_Gear.m
@@ -55,3 +55,45 @@ alpha_c = 10 +.3; % clearance angle
 AB = x_upsweep - x_MainGear; % distance from aft landing gear to upsweep (ft)
 Hf = AB*tand(alpha_c); % distance between landing gear and fuselage
 fprintf('The landing gear height is Hf %0.2f ft \n',Hf) 
+
+
+%% Tip-Over Criteria
+
+LG.geom.height = 6.34; % LG height, bottom of wheel to end of strut (ft)
+LG.geom.Bm = Bm; % distance between cg and main gear (ft)
+
+% Longitudinal Tip-Over Criterion
+% Roskam Pt 2, p.219, Fig 9.1a
+% Angle between most aft cg and vertical line extending from center of main
+% gear wheel (degrees)
+LG.geom.beta = atand(LG.geom.Bm / LG.geom.height); 
+LG.crit.LT_Tip = 15; % longitudinal tip-over criteria, min angle (degrees) 
+% Check if criterion is satisfied
+if LG.geom.beta > LG.crit.LT_Tip; 
+    fprintf('The longitudinal tip-over criterion is satisfied. \n')
+else
+    fprintf('The longitudinal tip-over criterion is NOT satisfied. \n')
+end
+
+% Lateral Tip-Over Criterion
+LG.crit.Lat_Tip = 55; % lateral tip-over criteria, max angle (degrees)
+
+
+%% Ground Clearance Criteria
+
+% Note: Both Longitudinal and Lateral ground clearance requirements must be
+% satisfied for tires and struts deflated. 
+
+% Longitudinal Ground Clearance Criterion
+%LG.geom.tail  % dist from ground to tail at end of fuselage (ft)
+x_MGaft = L_F - B - x_NoseGear; % dist from main gear to end of fuselage (ft)
+LG.crit.LT_Ground = alpha_TO; % takeoff angle 
+LG.geom.theta = atand(LG.geom.tail / x_MGaft); % tail clearance angle (degrees)
+% Check if criterion is satisfied
+if LG.geom.theta > LG.crit.LT_Ground;
+    fprintf('The longitudinal ground clearance criterion is satisfied. \n')
+else 
+    fprintf('The longitudinal ground clearance criterion is NOT satisfied. \n')
+end
+
+% Lateral Ground Clearance Criterion
