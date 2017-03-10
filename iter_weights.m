0e74468488c16dcd5e0462313eb1c937f9f81372
Andrew Chuen <amchuen@ucdavis.edu>
2017-03-10 14:42:00 -0800

    Added Preliminary Weight Iteration Script

---
+++ iter_weights.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+close all;
+
+%% INFO:
+
+% want to iterate weights to get new weight
+
+%% First Run of SAD.m
+
+Supersonic_Aircraft_Design;
+
+% Get corrected weight
+Class_2_Weights;
+
+% Calculate Residual
