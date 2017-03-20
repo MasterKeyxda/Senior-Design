66a00787fe12689601a5673ffca00bf530359ae9
mjbauer03 <mjbauer@ucdavis.edu>
2017-03-20 16:24:25 -0700

    Correct CDO

--- Drag_Analysis.m
+++ Drag_Analysis.m
@@ -48,7 +48,6 @@ fM = 1 - (0.08*req.cr_M0(1)^1.45);
 %-----Wing-----%
 
 % Eqn 3.19
-
 Swet.wing = WING.geom.S_area - 2*0.5*D_C*(WING.geom.sub.Cr +...
     (WING.geom.sub.Cr+(0.5*D_C)*((WING.geom.sub.Ct-WING.geom.sub.Cr)/WING.geom.sub.span)))/2; % wing wetted area correction for removed area inside fuselage
 Swet.wing = 2*Swet.wing; % wing wetted area
