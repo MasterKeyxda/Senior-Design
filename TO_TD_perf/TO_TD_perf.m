950a199ac9272b945f16ec7ce6017da053e800e5
Andrew Chuen <amchuen@ucdavis.edu>
2017-03-20 22:16:19 -0700

    Added takeoff and landing performance calculations.

--- TO_TD_perf/TO_TD_perf.m
+++ TO_TD_perf/TO_TD_perf.m
@@ -39,6 +39,8 @@ addpath('TO_TD_perf\');
 
 %% Take-off Ground Roll (S_NGR + S_R)
 
+fprintf('\nTakeoff Calculations\n');
+
 % Flight Conditions
 P_ref = 2116; % lbf/ft^2
 T_ref = 518.69; % Rankine
@@ -47,6 +49,7 @@ rho = 0.0023769; % slugs/ft^2
 gc = 32.174;
 mu_g = 0.25; % typical constant for asphalt/tarmac
 phi = 0.0; % runway gradient
+Vw = 0 * 1.467; % mph -> ft/s
 
 Thr = constraints.req_Thr; % ASSUMPTION HERE... CONSTANT THRUST! 
 
@@ -58,7 +61,7 @@ figs.fig11 = figs.fig11(find([diff(figs.fig11(:,1)); 1]),:);
 h_start = 5; % ft above ground
 AR_eff = @(h) WING.geom.AR / (spline(figs.fig11(:,1)', figs.fig11(:,2)', 2*h/WING.geom.span));
 alf0 = 2*pi/180; % 2 radians
-CD0 = 0.0204;
+CD0 = 0.0196;
 N_nose = 0.25;
 Nose_arm = 50; % ft from CG
 % Main_arm = 
@@ -78,7 +81,7 @@ Xpos(1,:) = [0.0, 0.0, 0.0]; % [X0, V0, t]
 ii = 1; % iterate with this
 L_i = 0.0;
 
-while (L_i < Wt.WTO)
+while (L_i < Wt.WTO) || (Xpos(ii,2) < 1.2*constraints.Vstall)
     ii = ii + 1;
     
     % Lift coefficient model
@@ -88,11 +91,11 @@ while (L_i < Wt.WTO)
     CLi = CL_ag*(alf0-dAlf_0g_i) + dCL; 
     % missing dCl for flaps at various AOA
     % Also missing loss of ground effect due to flaps
-    L_i = L_g(CLi, Xpos(ii-1, 2));
+    L_i = L_g(CLi, Xpos(ii-1, 2)+  1.5*Vw);
     
     % Drag Coefficient Model
     Cdi = CD0 + CLi^2 / (pi * AR_eff(h_start) * 0.9) + d_CDi_g(h_start, CLi, AR_eff(h_start));
-    D_i = D_g(Cdi, Xpos(ii-1, 2));
+    D_i = D_g(Cdi, Xpos(ii-1, 2) + 1.5*Vw);
     
     % Ground friction force
     if Wt.WTO > L_i
@@ -161,7 +164,7 @@ h_trans = h_trans + (dt*V_ii)*sin(gamma(2));
 dS_trans = dS_trans + (dt*V_ii)*cos(gamma(2));
 cleared = 0;
 
-while gamma(iii - ii) < hld.gamma_TO
+while h_trans < 35 
     iii = iii + 1;
     
     if (iii - ii) > length(alpha_schedule) && cleared
@@ -176,7 +179,7 @@ while gamma(iii - ii) < hld.gamma_TO
     CL_ag = CL_alf(AR_eff(h_trans), Xpos(iii-1,2)/a0); % Ground effect lift coefficient
     dAlf_0g_i = d_Alf_0g(h_trans);
     
-    CLi = CL_ag*(alpha_i1 + alf0 -dAlf_0g_i) + dCL; 
+    CLi = CL_ag*((alpha_i1-gamma(iii-ii-1)) + alf0 -dAlf_0g_i) + dCL; 
     % missing dCl for flaps at various AOA
     % Also missing loss of ground effect due to flaps
     L_i = L_g(CLi, Xpos(iii-1, 2));
@@ -185,8 +188,8 @@ while gamma(iii - ii) < hld.gamma_TO
     Cdi = CD0 + CLi^2 / (pi * AR_eff(h_trans) * 0.9) + d_CDi_g(h_trans, CLi, AR_eff(h_trans));
     D_i = D_g(Cdi, Xpos(iii-1, 2));
     
-    gamma(iii - ii) = gc / (Xpos(iii - 1,2)*Wt.WTO) * (Thr * sin(alpha_i1) + L_i - Wt.WTO*cos(gamma(iii-ii-1)));
-    dV = dt*(gc/Wt.WTO)*(Thr*cos(alpha_i1) - D_i - Wt.WTO*sin(gamma(iii-ii-1)));
+    gamma(iii - ii) = gc / (Xpos(iii - 1,2)*Wt.WTO) * (Thr * sin((alpha_i1-gamma(iii-ii-1))) + L_i - Wt.WTO*cos(gamma(iii-ii-1)));
+    dV = dt*(gc/Wt.WTO)*(Thr*cos((alpha_i1-gamma(iii-ii-1))) - D_i - Wt.WTO*sin(gamma(iii-ii-1)));
     V_ii = Xpos(iii-1,2) + dV;
     t_ii = dt + Xpos(iii-1, 3);
     X_ii = Xpos(iii-1, 1) + dt * Xpos(iii-1, 2);
@@ -208,4 +211,80 @@ end
 fprintf('Time to Climb-Out: %0.5f\n', Xpos(end,3) - Xpos(ii, 3));
 fprintf('V_INCL: %0.5f\n', Xpos(end, 2));
 fprintf('Transition Distance: %0.5f\n', dS_trans);
+fprintf('Final flight path angle: %0.5f deg\n', gamma(end)*180/pi);
 
+
+%% Landing Calcs -> Approach
+
+fprintf('\nLanding Calculations\n');
+% Requirement velocities according to FAR 25
+V_A = 1.15*constraints.Vstall;
+V_TD = 1.1 * V_A;
+V_FL = 0.95*V_A;
+gam_A = -1.5*pi/180; % degrees, approach angle
+h_screen = 50;
+n_FL = 1.04;
+
+fprintf('Approach Velocity: %0.2f ft/s\n', V_A);
+fprintf('Flare Velocity: %0.2f ft/s\n', V_FL); 
+fprintf('Touchdown Velocity: %0.2f ft/s\n', V_TD);
+fprintf('Approach Angle: %0.2f deg\n', -gam_A*180/pi);
+
+% Approach Calculations
+WT_land = Wt.WTO * Wt.fuel.w2_1 * Wt.fuel.w3_2 * Wt.fuel.w4_3 * Wt.fuel.w5_4 * Wt.fuel.w6_5;
+CL_A = 2* WT_land /(rho * WING.geom.S_area * V_A^2);
+CD_A = CD0 + CL_A^2 / (pi * WING.geom.AR * 0.9);
+Thr_land = (CD_A/CL_A - gam_A ) * WT_land;
+fprintf('Required Landing Thrust: %0.2f lbf\n', Thr_land);
+R_flare = V_FL^2 / (gc * (n_FL - 1));
+h_flare = R_flare * (1 - cos(gam_A));
+
+S_LTR = -R_flare * gam_A;
+S_LDES = max((h_screen - h_flare)/tan(gam_A), 2*h_flare / gam_A);
+
+% Ground Roll Calculations
+X_LR = [0.0, V_TD, 0.0];
+ii = 1;
+mu_gBrakes = 0.5*(0.65+0.8);
+N_n = 0.0;
+
+while X_LR(end, 2) > 0
+    ii = ii + 1;
+    
+    % Lift coefficient model
+    CL_ag = CL_alf(AR_eff(h_start), X_LR(ii-1,2)/a0); % Ground effect lift coefficient
+    dAlf_0g_i = d_Alf_0g(h_start);
+    
+    CLi = CL_ag*(alf0-dAlf_0g_i) + dCL; 
+    % missing dCl for flaps at various AOA
+    % Also missing loss of ground effect due to flaps
+    L_i = L_g(CLi, X_LR(ii-1, 2)+  1.5*Vw);
+    
+    % Drag Coefficient Model
+    Cdi = CD0 + CLi^2 / (pi * AR_eff(h_start) * 0.9) + d_CDi_g(h_start, CLi, AR_eff(h_start));
+    D_i = D_g(Cdi, X_LR(ii-1, 2) + 1.5*Vw);
+    
+    if L_i > WT_land
+        GF = 0;
+    else
+        GF = WT_land - L_i; 
+    end
+    
+    dV = dt*(gc/WT_land)*( - D_i - (mu_gBrakes)*(GF) - WT_land*sin(phi) + N_n*(mu_gBrakes - mu_g));
+    V_ii = X_LR(ii-1,2) + dV;
+    t_ii = dt + X_LR(ii-1, 3);
+    X_ii = X_LR(ii-1, 1) + dt * X_LR(ii-1, 2);
+    
+    X_LR(ii, :) = [X_ii, V_ii, t_ii];
+    
+    if (L_i < WT_land) && (N_n == 0)
+        fprintf('Main Landed for Touchdown at Time: %0.2fs\n', X_LR(ii, 3));
+        fprintf('Main Landing Position from TD Point: %0.2f ft\n', X_LR(ii,1));
+        N_n = 0.08 * WT_land;
+    end
+end
+
+fprintf('Total Air Landing Distance: %0.2f ft\n', S_LTR + S_LDES);
+fprintf('Total Ground Landing Distance: %0.2f ft\n', X_LR(end,1));
+S_L_tot = X_LR(end,1) + S_LTR + S_LDES;
+fprintf('Total Landing Distance: %0.2f ft\n', S_L_tot);
